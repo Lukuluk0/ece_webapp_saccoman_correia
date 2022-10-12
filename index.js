d42d64f0789b8edd4cb6f9246c11672fd34ab0c5
Alexandre CORREIA <alexandre.correia.m7@gmail.com>
2022-10-12 14:27:00 +0200

    init express

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // Import a module
-const http = require('http')
+//const http = require('http')
 
-const {serverHandle} = require('./handles')
+//const {serverHandle} = require('./handles')
 
-const port = "8080"
+//const port = "8080"
 
-const callback = () =>
-{
-    console.log("Server ir running at http::/localhost :" + port)
-}
+//const callback = () =>
+//{
+    //console.log("Server ir running at http::/localhost :" + port)
+//}
 
-http.createServer(serverHandle).listen(port, callback)
+//http.createServer(serverHandle).listen(port, callback)
+
+const port = 3000
+const express = require('express')
+const app = express()
+const birds = require('./birds')
+
+app.use('/app', app)
+
+  
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })

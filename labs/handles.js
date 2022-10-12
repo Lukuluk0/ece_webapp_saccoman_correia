db6d1d2448ddfc6d74728775afc33bc703a6e30c
Alexandre CORREIA <alexandre.correia.m7@gmail.com>
2022-10-12 15:08:02 +0200

    docs: ajout de page html en dehors fichier js

--- labs/handles.js
+++ labs/handles.js
@@ -3,32 +3,6 @@ const qs = require('querystring')
 
 const port = "8080"
 
-const profil = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' + 
-'    <body>' +
-'       <h1>Welcome in my profile page</h1>' +
-'       <p>My name is Alexandre Correia, I am 21 years old and I study at ECE in SI major</p>' +
-'       <p>My name is Lucas Saccoman, I am 21 years old and I study at ECE in SI major</p>'  +
-'    </body>' +
-'</html>'
-
-const hello = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' + 
-'    <body>' +
-'       <h1>Voici comment hello fonctionne :</h1>' +
-'       <p>Pour accéder à la page hello, il suffit de rentrer hello et votre nom dans l adresse url de la manière suivante:<br><a href="http://localhost:8080/hello?name=John">localhost:8080/hello?name=your_name</a></p>'  +
-'       <p>Pour accéder à la page du créateur, appuyer sur ce lien:<br><a href="http://localhost:8080/hello?name=ALEXANDRE">localhost:8080/hello?name=ALEXANDRE</a></p>'  +
-'    </body>' +
-'</html>'
-
 const error = '<!DOCTYPE html>' +
 '<html>' +
 '    <head>' +

ef3963366116acb84d5b374694eb2b49403f390c
Alexandre CORREIA <alexandre.correia.m7@gmail.com>
2022-10-26 10:20:51 +0200

    style: adding tailwind styles on page articles

--- app/pages/articles.js
+++ app/pages/articles.js
@@ -4,9 +4,9 @@ const Articles = () => {
     return <div>
         <Navbar />
             <ol>
-                <li>Article 1</li><p>loremipsum</p>
-                <li>Article 2</li><p>loremipsum</p>
-                <li>Article 3</li><p>loremipsum</p>
+                <li>Article 1</li><p className="italic font-bold">loremipsum</p>
+                <li>Article 2</li><p className="italic font-bold">loremipsum</p>
+                <li>Article 3</li><p className="italic font-bold">loremipsum</p>
             </ol>
       
     </div>
--- app/pages/index.js
+++ app/pages/index.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+
 import Navbar from '../component/Navbar'
 
 export default function Home() {

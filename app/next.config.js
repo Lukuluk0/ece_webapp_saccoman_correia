c411a15bb05c4198297af4a0ed052424b664b35a
alexandre.correia <alexandre.correia.m7@gmail.com>
2022-12-17 21:37:04 +0100

    fix: resolve problem about of un-configured Host fot next/Image

--- app/next.config.js
+++ app/next.config.js
@@ -2,7 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+      },
+    ],
+  },
+}
 
 
 module.exports = nextConfig

7275808fb75d3cd519d92d74a681554eacf3ac30
alexandre.correia <alexandre.correia.m7@gmail.com>
2022-12-17 21:41:26 +0100

    fix: display picture with next/Image

--- app/pages/projects/index.js
+++ app/pages/projects/index.js
@@ -1,12 +1,12 @@
 import { supabase } from '../api/supabase'
 import Layout from '../../components/Layout'
 import Test_projectslist from '../../components/Test_projectslist'
-import Link from 'next/link'
+import Addproject from '../../components/Addproject'
 import Head from 'next/head'
 
 
 export default function Projects({projects}) {
-
+  
     return (
       <Layout>
         <Head>
@@ -14,15 +14,8 @@ export default function Projects({projects}) {
         <meta name="description" content="WebTech projects page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-       <Test_projectslist pprojects={projects}></Test_projectslist>
-       <div className="mt-12 flex gap-x-4 justify-center">
-                  <Link href='/projects/creation'>
-                  <a className="inline-block rounded-lg bg-bleu px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-bleu hover:bg-indigo-500 hover:ring-indigo-500">
-                    Add a project
-                    <span className="text-white" aria-hidden="true">&rarr;</span>
-                  </a>
-                  </Link>
-        </div>
+       <Test_projectslist pprojects={projects}/>
+       <Addproject/>
     </Layout>
    )
   }

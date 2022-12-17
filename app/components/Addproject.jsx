8fbb61fc01366b9039b754554b9bdaad41403c69
alexandre.correia <alexandre.correia.m7@gmail.com>
2022-12-17 21:39:00 +0100

    fix: display button to add a project according to the status of the user

---
+++ app/components/Addproject.jsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import UserContext from './UserContext'
+import { useContext } from 'react'
+
+const Addproject = () => {
+    const { user } = useContext(UserContext)
+    
+    if(user == undefined)
+    {
+    return (
+    <div className="mt-12 flex gap-x-4 justify-center">
+                  <button className="inline-block rounded-lg bg-bleu px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-bleu hover:bg-indigo-500 hover:ring-indigo-500 cursor-not-allowed">
+                  Add a project
+                  </button>
+        </div>
+    )
+    }
+    else{
+        return (
+        <div className="mt-12 flex gap-x-4 justify-center">
+                  <Link href='/projects/creation'>
+                  <button className="inline-block rounded-lg bg-bleu px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-bleu hover:bg-indigo-500 hover:ring-indigo-500" >
+                    Add a project
+                  </button>
+                  </Link>
+        </div>
+        )
+    }
+    
+}
+
+export default Addproject
